f7bcc9eab7f4bcab6c914b22885fbe899334735c
shibi <shibidewdrops7@gmail.com>
2017-04-21 19:29:40 +0530

    #Raji/shibi #3717 Set default nil for category in content factory

--- app/model/content_factory.rb
+++ app/model/content_factory.rb
@@ -3,7 +3,7 @@ require_relative 'content'
 module Hacienda
   class ContentFactory
 
-    def instance(id, content_data, type:, locale:, category:)
+    def instance(id, content_data, type:, locale:, category: nil)
       Content.build(id, content_data, type: type, locale: locale, category: category)
     end
   end
