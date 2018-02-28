44c02695dcff29bd88424017b5f24a2fbc6138df
Vivek Soundrapandi <vivekhas3@gmail.com>
2018-02-28 16:45:53 +0530

    Vivek/Saran #3891 added ability select multiple fields

--- app/query/query_options/select_query_option.rb
+++ app/query/query_options/select_query_option.rb
@@ -1,16 +1,35 @@
+# frozen_string_literal: true
+
 module Hacienda
+  # Class to select specific fields from result
   class SelectQueryOption
     def initialize(query_option_value)
       @query_option_value = query_option_value
     end
 
     def apply(content)
-      items = []
+      @query_option_value.split(',').length > 1 ? apply_multiple_select(content) : apply_single_select(content)
+    end
 
+    private
+
+    def apply_single_select(content)
+      items = []
       content.each do |content_item|
         items << content_item[@query_option_value.to_sym]
       end
+      items
+    end
 
+    def apply_multiple_select(content)
+      items = []
+      content.each do |content_item|
+        result = {}
+        @query_option_value.split(',').each do |query_field|
+          result[query_field.to_sym] = content_item[query_field.to_sym]
+        end
+        items << result
+      end
       items
     end
   end
--- spec/unit/query/query_options/select_query_option_spec.rb
+++ spec/unit/query/query_options/select_query_option_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../../unit_helper'
 require_relative '../../../../app/query/query_options/select_query_option'
 
@@ -5,10 +7,23 @@ module Hacienda
   module Test
     describe SelectQueryOption do
       it 'should select only the specified properties' do
-        result = SelectQueryOption.new('id').apply([{id: 'an_id', stuff: 'do not care'}, {id: 'another_id', stuff: 'do not care'}])
+        result = SelectQueryOption.new('id').apply([{ id: 'an_id', stuff: 'do not care' }, { id: 'another_id', stuff: 'do not care' }])
 
         expect(result.size).to eq 2
         expect(result.first).to eq('an_id')
+      end
+      it 'should select multiple  specified properties ' do
+        result = SelectQueryOption.new('id,name').apply([{ id: 'an_id', stuff: 'do not care', name: 'test_name1' }, { id: 'another_id', stuff: 'do not care', name: 'test_name2' }])
+
+        expect(result.size).to eq 2
+        expect(result).to eq([{ id: 'an_id', name: 'test_name1' }, { id: 'another_id', name: 'test_name2' }])
+      end
+
+      it 'should not fail when select has unknwon fields ' do
+        result = SelectQueryOption.new('id,unknown').apply([{ id: 'an_id', stuff: 'do not care', name: 'test_name1' }, { id: 'another_id', stuff: 'do not care', name: 'test_name2' }])
+
+        expect(result.size).to eq 2
+        expect(result).to eq([{id: "an_id", unknown: nil}, {id: "another_id", unknown: nil}])
       end
     end
   end
